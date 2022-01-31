02944181a9330440535b26c8645ec71c1c52ca2a
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-31 11:51:16 -0300

    update api url

--- constants.js
+++ constants.js
@@ -1,28 +1,68 @@
 import Constants from "expo-constants";
-const { manifest } = Constants;
+const {manifest} = Constants;
 
-export const uri= `http://${manifest.debuggerHost.split(':').shift()}:4000`;
+//export const uri = `http://${manifest.debuggerHost.split(":").shift()}:4000`;
 
-export const abrMonths=['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL','AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+export const uri = "https://happyday.nicolasdeheza.com";
 
-export const abrMonthsCap= [ "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+export const abrMonths = [
+	"JAN",
+	"FEB",
+	"MAR",
+	"APR",
+	"MAY",
+	"JUN",
+	"JUL",
+	"AUG",
+	"SEP",
+	"OCT",
+	"NOV",
+	"DEC"
+];
 
-export const monthNames=['January', 'February', 'March', 'April', 'May', 'June', 'July',
-'August', 'September', 'October', 'November', 'December'];
+export const abrMonthsCap = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec"
+];
 
-export const monthColors= [
-    '#216b8e',
-    '#27a3a3',
-    '#149e84',
-    '#00ae4d',
-    '#69d503',
-    '#d6df24',
-    '#edc01c',
-    '#faa818',
-    '#f19616',
-    '#fd6126',
-    '#9458a9',
-    '#6258a2'
-]
+export const monthNames = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+export const monthColors = [
+	"#216b8e",
+	"#27a3a3",
+	"#149e84",
+	"#00ae4d",
+	"#69d503",
+	"#d6df24",
+	"#edc01c",
+	"#faa818",
+	"#f19616",
+	"#fd6126",
+	"#9458a9",
+	"#6258a2"
+];
 
 //export const uri= 'http://192.168.0.252:4000';
